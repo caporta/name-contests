72691bc82a262a169c6641f138635910253b8241
caporta <caporta@users.noreply.github.com>
2017-12-05 23:25:54 -0500

    Use humps lib to camelCase-ify response object keys

--- schema/types/me.js
+++ schema/types/me.js
@@ -9,6 +9,9 @@ module.exports = new GraphQLObjectType({
   name: 'MeType',
   fields: {
     id: { type: GraphQLID },
+    firstName: { type: GraphQLString },
+    lastName: { type: GraphQLString },
     email: { type: new GraphQLNonNull(GraphQLString) },
+    createdAt: { type: GraphQLString },
   },
 });
