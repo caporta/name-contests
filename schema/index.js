c89f050ab829c4e313d90ab74acde621b7de95f5
caporta <caporta@users.noreply.github.com>
2017-12-05 22:29:04 -0500

    Create custom type for response field

--- schema/index.js
+++ schema/index.js
@@ -1,20 +1,29 @@
 const {
   GraphQLSchema,
   GraphQLObjectType,
-  GraphQLString
-} = require('graphql')
+  GraphQLString,
+  GraphQLNonNull,
+} = require('graphql');
+
+const MeType = require('./types/me');
 
 // defines root selection scope
 const RootQueryType = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
-    hello: {
-      type: GraphQLString,
-      description: 'The "mandatory" hello world example, GraphQL style.',
-      resolve: () => 'world',
-    }
+    me: {
+      type: MeType,
+      description: 'The current user identified by an api key',
+      args: {
+        key: { type: new GraphQLNonNull(GraphQLString) },
+      },
+      resolve: () => {
+        // read user info from db
+      },
+    },
   },
 });
+
 const ncSchema = new GraphQLSchema({
   query: RootQueryType,
   // mutation: ...,
